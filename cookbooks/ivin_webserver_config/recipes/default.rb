bf2d6bbab31298a73dfd39fe85b31ce839f39cee
Ravi Sankar <ravi@chronus.com>
2013-05-16 14:24:45 +0530

    nginx conf repetition removed [#45143339] [#46105923]

--- cookbooks/ivin_webserver_config/recipes/default.rb
+++ cookbooks/ivin_webserver_config/recipes/default.rb
@@ -7,6 +7,35 @@
 # All rights reserved - Do Not Redistribute
 #
 
+nginx_path = node[:passenger][:path]
+nginx_log_path = node[:passenger][:nginx_log_path]
+tuned_ruby_path = node[:passenger][:tuned_ruby_path]
+
+template "#{nginx_path}/conf/nginx.conf" do
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(    
+    :nginx_log_path => nginx_log_path,    
+    :passenger_root => "##PASSENGER_ROOT##",
+    :ruby_path => tuned_ruby_path,
+    :passenger => node[:passenger],
+    :pidfile => "#{nginx_path}/logs/nginx.pid"
+  )
+  notifies :run, 'bash[config_patch]'
+end
+
+bash "config_patch" do
+  # The big problem is that we can't compute the gem install path
+  # because we don't know what ruby version we're being installed
+  # on if RVM is present.
+  # only_if "grep '##PASSENGER_ROOT##' #{nginx_path}/conf/nginx.conf"
+  user "root"
+  code "#{nginx_path}/sbin/config_patch.sh #{nginx_path}/conf/nginx.conf"
+  notifies :reload, 'service[passenger]'
+end
+
 template "#{node.default[:passenger][:path]}/conf/sites.d/ivin.conf" do
   source "rails_nginx_passenger.conf.erb"
   mode "644"
--- cookbooks/passenger/recipes/daemon.rb
+++ cookbooks/passenger/recipes/daemon.rb
@@ -73,35 +73,10 @@ directory "#{nginx_path}/conf/sites.d" do
   notifies :reload, 'service[passenger]'
 end
 
-template "#{nginx_path}/conf/nginx.conf" do
-  source "nginx.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  variables(    
-    :nginx_log_path => nginx_log_path,    
-    :passenger_root => "##PASSENGER_ROOT##",
-    :ruby_path => tuned_ruby_path,
-    :passenger => node[:passenger],
-    :pidfile => "#{nginx_path}/logs/nginx.pid"
-  )
-  notifies :run, 'bash[config_patch]'
-end
-
 cookbook_file "#{nginx_path}/sbin/config_patch.sh" do
   owner "root"
   group "root"
   mode 0755
-end
-
-bash "config_patch" do
-  # The big problem is that we can't compute the gem install path
-  # because we don't know what ruby version we're being installed
-  # on if RVM is present.
-#  only_if "grep '##PASSENGER_ROOT##' #{nginx_path}/conf/nginx.conf"
-  user "root"
-  code "#{nginx_path}/sbin/config_patch.sh #{nginx_path}/conf/nginx.conf"
-  notifies :reload, 'service[passenger]'
 end
 
 template "/etc/init.d/passenger" do
