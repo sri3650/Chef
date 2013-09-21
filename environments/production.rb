c46360379932f0b161baf923cfbade0be7d211e9
Arun Kumar Arjunan <arun@chronus.com>
2013-09-21 12:03:54 +0530

    [IVIN-252] Installing the sellers subdomain using Chef

--- environments/production.rb
+++ environments/production.rb
@@ -4,6 +4,7 @@ description "Production Environment of IVIN"
 default_attributes "ivin_application" => {
   "branch" => "r3_production",
   "server_name" => "ivin.intven.com",
+  "sellers_server_name" => "sellers.intven.com",
   "smtp_sasl_mailgun_user_name" => "postmaster@intven.com",
   "smtp_sasl_mailgun_passwd" => "62v32xayva-4"
 }
--- environments/staging.rb
+++ environments/staging.rb
@@ -4,6 +4,7 @@ description "Staging Environment of IVIN"
 default_attributes "ivin_application" => {
   "branch" => "r3_staging",
   "server_name" => "ivin.chronus.com",
+  "sellers_server_name" => "sellers.chronus.com",
   "smtp_sasl_mailgun_user_name" => "postmaster@chronus.com",
   "smtp_sasl_mailgun_passwd" => "7ebn-8t4iqa0"
 }
