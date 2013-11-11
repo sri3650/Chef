9e9dcf271302a2aa6d07d8f6b4f12235ae2bd19e
Arun Kumar Arjunan <arun@chronus.com>
2013-11-11 15:41:42 +0530

    Upgrading passenger

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
-gem 'chef', "11.4.0"
+gem 'chef', "11.8.0"
 gem "knife-ec2", "0.6.4"
--- cookbooks/passenger/recipes/install.rb
+++ cookbooks/passenger/recipes/install.rb
@@ -3,13 +3,13 @@
 # Recipe:: install
 
 gem_package "passenger/system" do
-  version '3.0.11'
+  version '4.0.23'
   package_name 'passenger'
   not_if "test -e /usr/local/bin/rvm-gem.sh"
 end
 
 gem_package "passenger/rvm" do
-  version '3.0.11'
+  version '4.0.23'
   package_name 'passenger'
   gem_binary "/usr/local/bin/rvm-gem.sh"
   only_if "test -e /usr/local/bin/rvm-gem.sh"
