7ba4ea712067101cad22fb9368e7a6649a8b63d9
Balaji Seetharaman <balaji@chronus.com>
2013-12-11 16:38:46 +0530

    commit
    changes to db clinet role [INC-368]

--- roles/db_client.rb
+++ roles/db_client.rb
@@ -3,6 +3,6 @@ name "db_client"
 description "Cloud Database of IVIN"
 
 run_list [
-  "recipe[mysql][client]",
-  "recipe[mysql_full_backup]"
+  "recipe[mysql::client]",
+  "recipe[database_backup::mysql_full_backup]"
 ]
