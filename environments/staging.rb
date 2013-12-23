799c21e4fcb79efffbbaec0ed9d0989e12c1010d
Balaji Seetharaman <balaji@chronus.com>
2013-12-23 16:16:12 +0530

    Updating certificate information for idf subdomain - IDF-403

--- environments/staging.rb
+++ environments/staging.rb
@@ -5,6 +5,7 @@ default_attributes "ivin_application" => {
   "branch" => "r3_staging",
   "server_name" => "ivin.chronus.com",
   "sellers_server_name" => "sellers.chronus.com",
+  "idf_server_name" => "idf.chronus.com",
   "smtp_sasl_mailgun_user_name" => "postmaster@chronus.com",
   "smtp_sasl_mailgun_passwd" => "7ebn-8t4iqa0"
 }
