6a6df527ec6d33201d61c8313ab3ad4c99629370
Vinu Devarajan <vinu.devarajan@chronus.com>
2014-01-24 15:41:39 +0530

    Upgrading Ruby and Passenger

--- cookbooks/passenger/recipes/install.rb
+++ cookbooks/passenger/recipes/install.rb
@@ -3,13 +3,13 @@
 # Recipe:: install
 
 gem_package "passenger/system" do
-  version '4.0.23'
+  version '4.0.35'
   package_name 'passenger'
   not_if "test -e /usr/local/bin/rvm-gem.sh"
 end
 
 gem_package "passenger/rvm" do
-  version '4.0.23'
+  version '4.0.35'
   package_name 'passenger'
   gem_binary "/usr/local/bin/rvm-gem.sh"
   only_if "test -e /usr/local/bin/rvm-gem.sh"
