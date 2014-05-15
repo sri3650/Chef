1979a177e3d697f7e90629c76a2ed805988d50d9
premanandh <prem.anandh@chronus.com>
2014-05-15 16:44:26 +0530

    INC-563 rotating keys in s3

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -20,7 +20,7 @@ chef_server_url          "https://api.opscode.com/organizations/#{org_name}"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
-knife[:aws_access_key_id]     = "AKIAJ7IQBBZD4AHQXIDA"
-knife[:aws_secret_access_key] = "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"
+knife[:aws_access_key_id]     = "AKIAJE2B5LGECCP4EGEA"
+knife[:aws_secret_access_key] = "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"
 knife[:distro] = "ubuntu12.04-ruby20"
 chef_client_path '/usr/local/bin/chef-client'
