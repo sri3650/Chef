813fa1bf893a039658015dd0266f081bc1077170
premanandh <prem.anandh@chronus.com>
2014-06-18 13:10:25 +0530

    INC-376 credential changes for new domain

--- environments/standby.rb
+++ environments/standby.rb
@@ -3,10 +3,10 @@ description "Standby Environment of IVIN"
 
 default_attributes "ivin_application" => {
   "branch" => "r3_standby",
-  "server_name" => "ivinstandby.chronus.com",
-  "sellers_server_name" => "sellersstandby.chronus.com",
-  "idf_server_name" => "idfstandby.chronus.com",
-  "smtp_sasl_mailgun_user_name" => "postmaster@chronus.com",
-  "smtp_sasl_mailgun_passwd" => "7ebn-8t4iqa0",
-  "mailgun_domain_name" => "chronus.com"
+  "server_name" => "ivinstandby.ivinchronus.com",
+  "sellers_server_name" => "sellersstandby.ivinchronus.com",
+  "idf_server_name" => "idfstandby.ivinchronus.com",
+  "smtp_sasl_mailgun_user_name" => "postmaster@ivinchronus.com",
+  "smtp_sasl_mailgun_passwd" => "659pmhlqj718",
+  "mailgun_domain_name" => "ivinchronus.com"
 }
