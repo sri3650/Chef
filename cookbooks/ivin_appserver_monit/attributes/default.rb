e4a39e5a78cb521390ba9fd263e0a7fd6cdb3cbf
premanandh <prem.anandh@chronus.com>
2014-07-08 18:53:14 +0530

    INC-538 removing thinking sphinx monit

--- cookbooks/ivin_appserver_monit/attributes/default.rb
+++ cookbooks/ivin_appserver_monit/attributes/default.rb
@@ -1,3 +1,3 @@
 include_attribute "ivin_common_dirs"
 
-default[:ivin_appserver_monit][:processes]    = %w[delayed_job redis searchd localeapp]
+default[:ivin_appserver_monit][:processes]    = %w[delayed_job redis localeapp]
