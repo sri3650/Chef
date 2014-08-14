e0f03b7743b82ff2c3dcf6e5d2d5534203e67f10
vikramjagirdar <Vikram.jagirdar@chronus.com>
2014-08-14 16:59:29 +0530

    INC-747 creating databags

--- cookbooks/ivin_ssl_certs/recipes/default.rb
+++ cookbooks/ivin_ssl_certs/recipes/default.rb
@@ -18,32 +18,42 @@ execute "Create the ssl cert directory" do
   not_if { ::File.exists?("#{ssl_cert_path}/cert/") }
 end
 
-cookbook_file "#{ssl_cert_path}/cert/#{node[:ivin_application][:server_name]}.crt" do
-  source "ssl_credentials/#{node.chef_environment}/#{node[:ivin_application][:server_name]}.crt"
+secret = Chef::EncryptedDataBagItem.load_secret("/etc/chef/encrypted_data_bag_secret")
+temp = Chef::EncryptedDataBagItem.load("ssl_certs", "#{node.chef_environment}", secret)
+
+
+template "#{ssl_cert_path}/cert/#{node[:ivin_application][:server_name]}.crt" do
+  variables(:ivin_com_crt => temp["ivin_com_crt"])
+  source "ssl_credentials/#{node.chef_environment}/#{node[:ivin_application][:server_name]}.crt.erb"
   mode "644"
 end
 
-cookbook_file "#{ssl_cert_path}/private/ivin_#{node.chef_environment}.key" do
-  source "ssl_credentials/#{node.chef_environment}/ivin_#{node.chef_environment}.key"
+template "#{ssl_cert_path}/private/ivin_#{node.chef_environment}.key" do
+  variables(:ivin_key => temp["ivin_key"])
+  source "ssl_credentials/#{node.chef_environment}/ivin_#{node.chef_environment}.key.erb"
   mode "644"
 end
 
-cookbook_file "#{ssl_cert_path}/cert/#{node[:ivin_application][:sellers_server_name]}.crt" do
-  source "ssl_credentials/#{node.chef_environment}/#{node[:ivin_application][:sellers_server_name]}.crt"
+template "#{ssl_cert_path}/cert/#{node[:ivin_application][:sellers_server_name]}.crt" do
+ variables(:sellers_com_crt => temp["sellers_com_crt"])
+  source "ssl_credentials/#{node.chef_environment}/#{node[:ivin_application][:sellers_server_name]}.crt.erb"
   mode "644"
 end
 
-cookbook_file "#{ssl_cert_path}/private/sellers_#{node.chef_environment}.key" do
-  source "ssl_credentials/#{node.chef_environment}/sellers_#{node.chef_environment}.key"
+template "#{ssl_cert_path}/private/sellers_#{node.chef_environment}.key" do
+  variables(:sellers_key => temp["sellers_key"])
+  source "ssl_credentials/#{node.chef_environment}/sellers_#{node.chef_environment}.key.erb"
   mode "644"
 end
 
-cookbook_file "#{ssl_cert_path}/cert/#{node[:ivin_application][:idf_server_name]}.crt" do
-  source "ssl_credentials/#{node.chef_environment}/#{node[:ivin_application][:idf_server_name]}.crt"
+template "#{ssl_cert_path}/cert/#{node[:ivin_application][:idf_server_name]}.crt" do
+  variables(:idf_com_crt => temp["idf_com_crt"])
+  source "ssl_credentials/#{node.chef_environment}/#{node[:ivin_application][:idf_server_name]}.crt.erb"
   mode "644"
 end
 
-cookbook_file "#{ssl_cert_path}/private/idf_#{node.chef_environment}.key" do
-  source "ssl_credentials/#{node.chef_environment}/idf_#{node.chef_environment}.key"
+template "#{ssl_cert_path}/private/idf_#{node.chef_environment}.key" do
+  variables(:idf_key => temp["idf_key"])
+  source "ssl_credentials/#{node.chef_environment}/idf_#{node.chef_environment}.key.erb"
   mode "644"
 end
--- environments/standby.rb
+++ environments/standby.rb
@@ -1,16 +1,12 @@
 name "standby"
 description "Standby Environment of IVIN"
 
-# secret = Chef::EncryptedDataBagItem.load_secret("/etc/chef/encrypted_data_bag_secret")
-# sasl_mailgun_passwd= Chef::EncryptedDataBagItem.load("env_staging", "smtp_sasl_mailgun_passwd", secret)
 
 default_attributes "ivin_application" => {
   "branch" => "r3_standby",
   "server_name" => "ivinstandby.ivinchronus.com",
   "sellers_server_name" => "sellersstandby.ivinchronus.com",
   "idf_server_name" => "idfstandby.ivinchronus.com",
-  "smtp_sasl_mailgun_user_name" => "postmaster@ivinchronus.com",
-  "smtp_sasl_mailgun_passwd" => "659pmhlqj718",
-  # "smtp_sasl_mailgun_passwd" => sasl_mailgun_passwd["password"],
+  "smtp_sasl_mailgun_user_name" => "file_standby"
   "mailgun_domain_name" => "ivinchronus.com"
 }
