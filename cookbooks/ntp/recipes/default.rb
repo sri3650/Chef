f9ad229356a8a827b007c229c12241d4ae994b5e
vikramjagirdar <Vikram.jagirdar@chronus.com>
2014-08-25 19:28:15 +0530

    INC-759 complete restart of NTP

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -5,16 +5,20 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
 package 'ntp'
 
-cookbook_file 'etc/ntp.conf' do
-   source 'ntp.conf'
-   notifies :restart, 'services[ntp]'
+execute "restart_ntp" do
+  command "/etc/init.d/ntp restart"
 end
+
+cookbook_file '/etc/ntp.conf' do
+   source 'ntp.conf'
+    owner "root"
+    group "root"
+    mode "644"
+    notifies :restart, resources(:execute => "restart_ntp")
+ end
 
 service  'ntp' do
  action [:enable, :start]
-
-end 
-
+end
