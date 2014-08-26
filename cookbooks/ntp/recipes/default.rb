b1e62234c6dcc276f9634df6259ecc84fbf5d12b
vikramjagirdar <Vikram.jagirdar@chronus.com>
2014-08-26 11:03:14 +0530

    INC-759 adding install option

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -5,20 +5,23 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-package 'ntp'
+package 'ntp' do
+	action: install
+end
 
 execute "restart_ntp" do
   command "/etc/init.d/ntp restart"
 end
 
 cookbook_file '/etc/ntp.conf' do
-   source 'ntp.conf'
-    owner "root"
-    group "root"
-    mode "644"
-    notifies :restart, resources(:execute => "restart_ntp")
- end
+  source 'ntp.conf'
+  owner "root"
+  group "root"
+  mode "644"
+  notifies :reload, 'service[ntp]'
+  # notifies :restart, resources(:execute => "restart_ntp")
+end
 
 service  'ntp' do
- action [:enable, :start]
+  action [:enable, :start]
 end
