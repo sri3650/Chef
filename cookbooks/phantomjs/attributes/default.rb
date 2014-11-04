dbcb0a60c813eae81e95e9c1707437bfc84b6fcd
Kumar <arunkumar@chronus.com>
2014-11-04 13:43:18 +0530

    Update phantomjs version

--- cookbooks/phantomjs/attributes/default.rb
+++ cookbooks/phantomjs/attributes/default.rb
@@ -1,4 +1,4 @@
-version = "1.9.8"
+version = "1.9.2"
 arch    = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i686"
 
 default[:phantomjs] = {
--- cookbooks/phantomjs/metadata.rb
+++ cookbooks/phantomjs/metadata.rb
@@ -1,3 +1,4 @@
 maintainer       "Travis CI Development Team"
 license          "Apache v2.0'"
 description      "Installs/Configures phantomjs"
+version           "0.0.1"
