4f7502cb567b843d911b2d9693a537fe5a24cf16
Vinu Devarajan <vinu.devarajan@chronus.com>
2014-12-31 12:14:55 +0530

    Enabling r4_staging for tddium cucumbers

--- cookbooks/ivin_common_dirs/files/default/cron_for_tddium_branches.rb
+++ cookbooks/ivin_common_dirs/files/default/cron_for_tddium_branches.rb
@@ -4,7 +4,7 @@ require 'git'
 require 'yaml'
 
 directory = "/mnt/app/cucumbers"
-cucumber_origin_branches = ["release_rails4", "staging", "r3_develop"]
+cucumber_origin_branches = ["release_rails4", "r4_staging", "r3_develop"]
 git = Git.init
 
 unless File.directory?(directory)
@@ -19,6 +19,7 @@ unless File.directory?(directory)
   end
 else
   git = Git.init(directory)
+  git.fetch
 end
 
 cucumber_origin_branches.each do |branch|
