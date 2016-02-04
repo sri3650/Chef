7ce07279b805c53bf464a3b2e322798d9a0ad88a
Mohamed Haleemullah <mohamed.haleemullah@chronus.com>
2016-02-04 14:26:06 +0530

    IDF-1493 install imagemagick 6.9.3-2

---
+++ cookbooks/ivin_imagemagick/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:imagemagick][:url] = 'https://www.imagemagick.org/download/ImageMagick-6.9.3-2.tar.gz'
+default[:imagemagick][:version] = '6.9.3-2'
---
+++ cookbooks/ivin_imagemagick/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: imagemagick
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "ark"
+
+ark 'imagemagick' do
+  url "#{node.default[:imagemagick][:url]}"
+  version "#{node.default[:imagemagick][:version]}"
+  action :config
+  action :install_with_make
+end
